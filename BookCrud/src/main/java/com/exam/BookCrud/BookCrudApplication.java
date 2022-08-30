289751dad85e6e9441fa45187901e69fb873561f
Adrian <adriancharles.soriano.iics@ust.edu.ph>
2022-08-30 21:21:43 +0800

    add

--- BookCrud/src/main/java/com/exam/BookCrud/BookCrudApplication.java
+++ BookCrud/src/main/java/com/exam/BookCrud/BookCrudApplication.java
@@ -1,27 +1,20 @@
 package com.exam.BookCrud;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookCrudApplication implements CommandLineRunner {
-	
-	@Autowired
-	private CategoryRepository catRepo;
+public class BookCrudApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookCrudApplication.class, args);
-	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		List<Category> categories = catRepo.findAll();
-		
-		categories.forEach(System.out :: println);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookCrudApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BookCrudApplication.class);
+    }
 }
